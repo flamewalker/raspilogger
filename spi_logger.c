f1842df364f24372e34cad26b050fe679b3d70ab
flamewalker <flamewalker@home.se>
2016-03-18 13:56:00 +0100

    Update spi_logger.c
    Tidy up version, still trying to get hang of releases...
    
    Error log gets filled up with errors, add code to decrease some of it

--- spi_logger.c
+++ spi_logger.c
@@ -1,6 +1,6 @@
 /*
  * SPI master interface to communicate with I2C datalogger
- * ver 0.8.0
+ * ver 0.8.1
  */
 
 #include <stdio.h>
@@ -182,9 +182,12 @@ void myInterrupt(void)
   // Check if SAMPLE_DUMP command was acknowledged
   if (buffer != 0x04)
   {
-    time_t now = time(NULL);
-    strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
-    fprintf(stderr, "%s %s %d\n", buff, "Sample collection returned error:", buffer);
+    if (buffer != 0x01)
+    {
+       time_t now = time(NULL);
+       strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+       fprintf(stderr, "%s %s %d\n", buff, "Sample collection returned error:", buffer);
+    }
     return;
   }
 
