a5e54052cad92347a03afdb3990f7d65a107c121
flamewalker <flamewalker@home.se>
2016-03-18 12:58:12 +0100

    Update spi_logger.c
    Massive edit, tested locally

--- spi_logger.c
+++ spi_logger.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdint.h>         // Needs this for uint8_t
+#include <time.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 #include <mysql/mysql.h>
@@ -15,54 +16,105 @@
 #define HISTORICAL 0x8B
 #define CURRENT 0xAC
 
-#define SPEED 500000        // SPI speed
-#define INT_PIN 24          // BCM pin for interrupt
+#define SPEED 1000000       // SPI speed
+#define INT_PIN 24          // BCM pin for interrupt from AVR
+#define RESET_PIN 25        // BCM pin for reset AVR
 
-uint8_t datalog[ARRAY_SIZE];
+
+// Watchdog flag to check if SPI connection is alive
+uint8_t conn_alive = 0;
+
+char buff[20];
 
 void myInterrupt(void);
 
-int main (void)
+void finish_with_error(MYSQL *conn);
+
+int main(void)
 {
-  printf("Raspberry Pi wiringPi SPI program\n");
+  time_t now = time(NULL);
+  strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+  fprintf(stdout, "Raspberry Pi SPI Master interface to AVR CTC-logger\n");
+  fprintf(stderr, "%s %s\n", buff, "Initialise error log");
 
   if (wiringPiSPISetup (0, SPEED) < 0)
   {
-    fprintf(stderr, "Unable to open SPI device 0: %s\n", strerror (errno));
+    now = time(NULL);
+    strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    fprintf(stderr, "%s %s\n", buff, "Unable to open SPI device 0: %s", strerror(errno));
     exit(1);
   }
 
-  printf("Starting\n");
   wiringPiSetupGpio();
-  wiringPiISR (INT_PIN, INT_EDGE_RISING, &myInterrupt) ;
-  printf("Waiting for interrupt. Press CTRL+C to stop\n");
-  for (;;)
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, HIGH);
+  pinMode(INT_PIN, INPUT);
+  pullUpDnControl(INT_PIN, PUD_DOWN);
+  if (wiringPiISR (INT_PIN, INT_EDGE_RISING, &myInterrupt) < 0)
   {
-
+    now = time(NULL);
+    strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    fprintf(stderr, "%s %s\n", buff, "Unable to register ISR: %s", strerror(errno));
+    exit(1);
+  }
+  fprintf(stdout, "Waiting for interrupt. Press CTRL+C to stop\n");
+  while (1)
+  {
+    sleep(65);
+    if (conn_alive == 1)
+      conn_alive = 0;
+    else
+    {
+      now = time(NULL);
+      strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+      fprintf(stderr, "%s %s\n", buff, "SPI connection has not been in use for last 65 seconds, reset AVR!");
+      digitalWrite(RESET_PIN, LOW);
+      sleep(1);
+      digitalWrite(RESET_PIN, HIGH);
+    }
   }
   return 0 ;
 }
 
+void finish_with_error(MYSQL *conn)
+{
+  time_t now = time(NULL);
+  strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+  fprintf(stderr, "%s %s %s\n", buff, mysql_error(conn));
+  mysql_close(conn);
+  exit(1);
+}
+
 void myInterrupt(void)
 {
-  FILE *fp;
-  MYSQL *conn;
-  conn = mysql_init(NULL);
-  MYSQL_RES *res;
-  MYSQL_ROW row;
-  char *server = "localhost";
-  char *user = "xxxxxx";
-  char *password = "xxxxxxx";
-  char *database = "ctclog";
 
+  uint8_t datalog[ARRAY_SIZE];
+
+  // Set watchdog flag for SPI connection alive
+  conn_alive = 1;
+
+  // Every C prog must have a x counter...
   uint8_t x;
+
+  // Transmission buffer for SPI, preloaded with SAMPLE_DUMP command
   unsigned char buffer = 0x04;
-  
+
   wiringPiSPIDataRW(0,&buffer,1);
   buffer = 0x00;
   wiringPiSPIDataRW(0,&buffer,1);
-  
-  for (x = 1; x < ARRAY_SIZE; x++)
+
+  // Check if SAMPLE_DUMP command was acknowledged
+  if (buffer != 0x04)
+  {
+    time_t now = time(NULL);
+    strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", localtime(&now));
+    fprintf(stderr, "%s %s\n", buff, "Sample collection returned error");
+    return;
+  }
+
+  // Loop for sampling entire array from AVR and store it
+  for (x = 0x01 ; x < ARRAY_SIZE ; x++)
   {
     buffer = x;
     wiringPiSPIDataRW(0,&buffer,1);
@@ -70,55 +122,74 @@ void myInterrupt(void)
   }
   buffer = 0xFF;
   wiringPiSPIDataRW(0,&buffer,1);
-  datalog[0xDB] = buffer;
-  
-  fp = fopen("settings.csv", "w+");
-  for (x = 0; x < SETTINGS ; x++)
-    fprintf(fp, "%d,", datalog[x]);
-  fprintf(fp, "%d", datalog[x++]);
+  datalog[ARRAY_SIZE-1] = buffer;
+
+  // Write downloaded array to files with some formatting to fit better into SQL database
+  // Start with SETTINGS table
+  FILE *fp;
+  fp = fopen("/tmp/settings.csv", "w");
+  fprintf(fp,"NULL,%f,", (float)datalog[0]/2);
+  for (x = 1 ; x < SETTINGS ; x++)
+  {
+    if (x ==0x06 || x ==0x07 || x == 0x13 || x == 0x15 || x == 0x1E || x == 0x1F || x == 0x3A)
+      fprintf(fp, "%d,", datalog[x]-40);
+    else
+      if (x == 0x16)
+        fprintf(fp, "%u%u%u,", datalog[x], datalog[x++], datalog[x++]);
+      else
+        fprintf(fp, "%u,", datalog[x]);
+  }
+  fprintf(fp, "%u", datalog[x]);
   fclose(fp);
 
-  fp = fopen("historical.csv", "w+");
-  for (; x < HISTORICAL ; x++)
-    fprintf(fp, "%d," , datalog[x]);
-  fprintf(fp, "%d", datalog[x++]);
+  // Continue with HISTORICAL table
+  fp = fopen("/tmp/historical.csv", "w");
+  fprintf(fp,"NULL,");
+  fprintf(fp,"%u,",datalog[0x73]);
+  fprintf(fp,"%02u:%02u,", datalog[0x74], datalog[0x75]);
+  fprintf(fp,"%02u%02u%02u,", datalog[0x78],datalog[0x77],datalog[0x76]);
+  fprintf(fp,"%02u%02u%02u,", datalog[0x7B],datalog[0x7A],datalog[0x79]);
+  for (x=0x7C ; x < 0x85 ; x++)
+      fprintf(fp, "%u," , datalog[x]);
+  fprintf(fp,"%02u:%02u,", datalog[0x86],datalog[0x85]);
+  fprintf(fp,"%u,", datalog[0x87]);
+  fprintf(fp,"%02u%02u%02u,", datalog[0x8A],datalog[0x89],datalog[0x88]);
+  fprintf(fp, "%u", datalog[0x8B]);
   fclose(fp);
 
-  fp = fopen("current.csv", "w+");
-  for (; x < CURRENT ; x++)
-    fprintf(fp, "%d," , datalog[x]);
-  fprintf(fp, "%d", datalog[x]);
+  // Finish with SETTINGS table
+  fp = fopen("/tmp/current.csv", "w");
+  fprintf(fp,"NULL,");
+  for (x=HISTORICAL+1 ; x < CURRENT-2 ; x++)
+  {
+    if (x == 0x8E || x == 0x94 || x == 0x95)
+      fprintf(fp, "%d," , datalog[x]-40);
+    else
+      if (x == 0x8F)
+        fprintf(fp, "%f,", (float)datalog[x++]+((float)datalog[x+1]/10));
+      else
+        fprintf(fp, "%u," , datalog[x]);
+  }
+  fprintf(fp, "%f,", (float)datalog[x++]/2);
+  fprintf(fp, "%u,", datalog[x++]);
+  fprintf(fp, "%u", datalog[x]);
   fclose(fp);
 
-  if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
-  {
-    fprintf(stderr, "%s\n", mysql_error(conn));
-    exit(1);
-  }
+  // MySQL magic happens here...
+  MYSQL *conn = mysql_init(NULL);
+  char *server = "localhost";
+  char *user = "xxx";
+  char *password = "xxx";
+  char *database = "ctclog";
 
-  if (mysql_query(conn, "LOAD DATA INFILE 'settings.csv' INTO TABLE SETTINGS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'"))
-  {
-    fprintf(stderr, "%s\n", mysql_error(conn));
-    exit(1);
-  }
+  if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, CLIENT_MULTI_STATEMENTS))
+    finish_with_error(conn);
 
-  if (mysql_query(conn, "LOAD DATA INFILE 'historical.csv' INTO TABLE SETTINGS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'"))
-  {
-    fprintf(stderr, "%s\n", mysql_error(conn));
-    exit(1);
-  }
-
-  if (mysql_query(conn, "LOAD DATA INFILE 'current.csv' INTO TABLE SETTINGS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'"))
-  {
-    fprintf(stderr, "%s\n", mysql_error(conn));
-    exit(1);
-  }
-
-  if (mysql_query(conn, "INSERT INTO TIME VALUES(CURDATE(),CURTIME())"))
-  {
-    fprintf(stderr, "%s\n", mysql_error(conn));
-    exit(1);
-  }
+  if (mysql_query(conn, "LOAD DATA INFILE '/tmp/settings.csv' INTO TABLE SETTINGS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' SET log_idx=null;\
+      LOAD DATA INFILE '/tmp/historical.csv' INTO TABLE HISTORICAL FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' SET log_idx=null;\
+      LOAD DATA INFILE '/tmp/current.csv' INTO TABLE CURRENT FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' SET log_idx=null;\
+      INSERT INTO TIME VALUES(NULL,NULL)"))
+    finish_with_error(conn);
 
   mysql_close(conn);
 }
